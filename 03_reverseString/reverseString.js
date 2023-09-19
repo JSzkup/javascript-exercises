72fc906383feaf1e24f3460ed574ea96724c4523
Jonathan Szkup <jszkup101@gmail.com>
2023-09-19 18:08:04 -0400

    Pass first test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (string) {
+    const arrayString = string.split('')
+    const arrayReversed = []
 
+    for (let i = 0; i < string.length; i++) {
+        arrayReversed.push(arrayString.pop());
+    }
+
+    return arrayReversed.join("")
 };
 
 // Do not edit below this line
