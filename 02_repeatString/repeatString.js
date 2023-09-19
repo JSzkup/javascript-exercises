7ad1d410b28d3796da86d4caceb60375d9093c08
Jonathan Szkup <jszkup101@gmail.com>
2023-09-19 17:42:16 -0400

    Add error checking for negative numbers

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -2,9 +2,12 @@ const repeatString = function (string, num) {
 
     let sentence = ""
 
-
-    for (let i = 0; i < num; i++) {
-        sentence += string;
+    if (num >= 0) {
+        for (let i = 0; i < num; i++) {
+            sentence += string;
+        }
+    } else {
+        return "ERROR"
     }
 
     return sentence
