d489c8c59ecd6a392990220f33cf3a944295da1c
Jonathan Szkup <jszkup101@gmail.com>
2023-09-19 17:35:47 -0400

    repeat string first test passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
 
+    let sentence = ""
+
+
+    for (let i = 0; i < num; i++) {
+        sentence += string;
+    }
+
+    return sentence
 };
 
 // Do not edit below this line
