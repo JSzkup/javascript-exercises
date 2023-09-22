661529c5963dfa65a3a22d2f58f27d43797b45e8
Jonathan Szkup <jszkup101@gmail.com>
2023-09-21 21:22:57 -0400

    complete 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,14 @@
-const leapYears = function() {
+const leapYears = function (year) {
+
+    // if year
+    // divisible by 4
+    // if its divisible by 100 its NOT a leap year UNLESS its ALSO divisible by 400
+
+    if ((year % 4 === 0) && (year % 100 !== 0 || year % 400 === 0)) {
+        return true;
+    }
+
+    return false;
 
 };
 
