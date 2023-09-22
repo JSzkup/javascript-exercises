49ae333d83476ec495e0d8b9b6e62d558a5a2795
Jonathan Szkup <jszkup101@gmail.com>
2023-09-21 20:33:08 -0400

    05_sumAll passes base test case

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function (lowerNum, upperNum) {
 
+    let sum = 0;
+
+    // for each number from x to y
+    for (lowerNum; lowerNum <= upperNum; lowerNum++)
+        sum += lowerNum
+
+    return sum;
 };
 
 // Do not edit below this line
