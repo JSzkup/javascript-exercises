1b46ed897615a2194296f10ab8d63e98f6fb4b25
Jonathan Szkup <jszkup101@gmail.com>
2024-01-29 20:48:09 -0500

    finish calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b
 };
 
-const sum = function() {
-	
+const sum = function (numList) {
+  let result = numList.reduce((total, current) => total + current, 0);
+  return result;
 };
 
-const multiply = function() {
+const multiply = function (numList) {
+  let result = numList.reduce((total, current) => total * current, 1);
 
+  return result;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  // create a list of num size of only num numbers
+  let factorialArray = [];
+  const originalNum = num;
+
+  for (let i = 0; i < originalNum; i++) {
+    factorialArray.push(num);
+    num -= 1;
+  }
+
+  const result = multiply(factorialArray)
+
+  return result;
 };
 
 // Do not edit below this line
