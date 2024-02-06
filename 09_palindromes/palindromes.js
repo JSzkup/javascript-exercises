ccf67d76dded9d7bb0db5a9cea6331300f6955c2
Jonathan Szkup <jszkup101@gmail.com>
2024-02-06 17:06:19 -0500

    complete palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    // reverse the string then check if the original is equal
+    // ignore punctuation, capitalization, and spaces
+    const processedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return processedString.split("").reverse().join("") == processedString;
 };
 
 // Do not edit below this line
