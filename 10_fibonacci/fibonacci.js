e457d76bb1031d3a39273cdd4e3cb9854fa012da
Jonathan Szkup <jszkup101@gmail.com>
2024-02-07 14:44:42 -0500

    fibonacci complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    if (num == 0) {
+        return 0;
+    } else if (num == 1 || num == 2) {
+        return 1;
+    } else if (num < 0) {
+        return "OOPS";
+    }
+
+    let first = 1
+    let second = 0
+
+    for (let i = 2; i <= num; ++i) {
+        let current = first + second;
+        second = first;
+        first = current;
+
+
+    }
+
+    return first;
 
 };
 
