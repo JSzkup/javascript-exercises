f0aab33d6e26878c999ea5ae8372396be9a21909
Jonathan Szkup <jszkup101@gmail.com>
2024-02-19 17:44:37 -0500

    complete findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function (peopleObjectArray) {
+
+    return peopleObjectArray.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+
+        // return if current age is older than oldest, else return oldest
+        return oldestAge < currentAge ? currentPerson : oldest;
+    });
+};
+
+
+function getAge(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    };
+
+    return death - birth;
 
 };
 
